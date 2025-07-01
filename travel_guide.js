2e95183c5880b8aec1c374b6b4629f1914855c85
Thaiyeba <riyaz20042047@gmail.com>
2025-07-01 11:31:39 -0400

    message

--- travel_guide.js
+++ travel_guide.js
@@ -40,9 +40,23 @@ function searchdest(e) {
                     });
                 }
                 
-                //if{
+                else if(normalized){    
+                    const country=data.countries.find(item=> item.name.toLowerCase() === normalized);
+                    if (country && country.cities){
+                        for (let i=0;i<country.cities.length;i++){
+                            const city=country.cities[i];
+                            resultdiv.innerHTML+=`
+                            <div style="background-color:white;border-radius:10px;padding:15px;box-shadow:0 0 10px rgba(0,0,0,0.2);width:620px;">
+                            <img src="${city.imageUrl}" alt="${city.name}" style="height:400px;width:100%;object-fit:cover;border-radius:10px;">
+                            <h2 style="color: rgb(1, 45, 92); font-family: Georgia; margin-top: 10px;">${city.name}</h2>
+                            <p style="color:#333;font-size:15px;">${city.description}</p>
+                            <button style="margin-top:10px;padding:8px 15px;background-color:rgb(1,45,92);color:white;border:none;border-radius:30px;">Visit</button>
+                            </div>
+                            `;
+                        }
+                    }
 
-                //}
+                }
                 else {
                     resultdiv.innerHTML = `<p style="color:white;">Try 'beach', 'temple', or a valid country name.</p>`;
                 }
@@ -54,4 +68,10 @@ function searchdest(e) {
     } else {
         alert("Invalid keyword. Enter a valid name.");
     }
+}
+const resetbtn=document.getElementById('resetbtn');
+resetbtn.addEventListener('click',clearsearch);
+function clearsearch(e){    
+    e.preventDefault();
+    document.getElementById('input').value="";
 }
